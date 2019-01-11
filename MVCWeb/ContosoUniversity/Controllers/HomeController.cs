d5dd5014cea58586e88b9cdc7b1b4af2f4b9673f
LGDisgod1 <1015475353@qq.com>
2019-01-11 13:17:41 +0800

    Tqi

---
+++ MVCWeb/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.comm;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           var data = WeaterHelper.GetWeatherByCityName("柳州");
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
